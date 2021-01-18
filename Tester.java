3018230cf8f2d153d726568eaa741fa0a2f58346
Max Han <mwellh84@gmail.com>
2021-01-18 10:27:16 -0500

    add cracker tester

--- Tester.java
+++ Tester.java
@@ -2,6 +2,7 @@ import edu.duke.*;
 import java.util.*;
 
 public class Tester {
+	// encrypt or decrypt from a stringified file
 	public void testCaesarCipher () {
 		int k = 4;
 		CaesarCipher cc = new CaesarCipher(k);
@@ -12,5 +13,18 @@ public class Tester {
 		System.out.println("the encrypted file is " + encrypted);
 		String decrypted = cc.decrypt(encrypted);
 		System.out.println("the decrypted file is " + decrypted);
+	}
+
+	// decrypt an encrypted string
+	public void testCracker () {
+		CaesarCracker cracker = new CaesarCracker();
+		int k = 4;
+		CaesarCipher cc = new CaesarCipher(k);
+		FileResource fr = new FileResource();
+		String fileString = fr.asString();
+		String encrypted = cc.encrypt(fileString);
+		String decryptedFromCracker = cracker.decrypt(encrypted);
+		System.out.println("the cracker has decrypted the message to be " + decryptedFromCracker);
+		
 	}
 }
